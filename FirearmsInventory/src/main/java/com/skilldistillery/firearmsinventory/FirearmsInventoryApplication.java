0a9e075b3c9b7415d8a41521b2a149cf178494d6
Dean Coffman <deansdetail@gmail.com>
2023-07-14 19:29:29 -0400

    creat and config boot mvc project with JPA Dependency

---
+++ FirearmsInventory/src/main/java/com/skilldistillery/firearmsinventory/FirearmsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.firearmsinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirearmsInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirearmsInventoryApplication.class, args);
+	}
+
+}
---
+++ FirearmsInventory/src/test/java/com/skilldistillery/firearmsinventory/FirearmsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.firearmsinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirearmsInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
